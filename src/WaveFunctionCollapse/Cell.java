a8f28d1c5ab01abab9f9eb1f63f303b13c656799
AdamDigby <4404AdBo19@toothillschool.co.uk>
2023-07-22 11:39:00 +0100

    First Attempt at a java wave function collapse algorithm

--- src/WaveFunctionCollapse/Cell.java
+++ src/WaveFunctionCollapse/Cell.java
@@ -4,8 +4,12 @@ public class Cell {
     int x;
     int y;
     boolean collapsed;
+    int color;
+
 
     boolean[] possibilities;
+    boolean[] demands;
+    int possibilitiesSum;
 
     public Cell(int X, int Y) {
         this.x = X;
@@ -14,10 +18,54 @@ public class Cell {
         // Initialises the possibilities coefficients to be all true.
         this.possibilities = new boolean[Main.distinctTileCount];
         for (int i = 0; i < Main.distinctTileCount; i++) {this.possibilities[i] = true;}
+        for(boolean possibility:this.possibilities) {
+            if (possibility) {this.possibilitiesSum++;}
+        }
+        // Initialises the possibilities coefficients to be all false.
+        this.demands = new boolean[Main.distinctTileCount];
+
+
 
     }
     public void collapse() {
+        int randomIndex = (int)Math.floor(Math.random() * this.possibilitiesSum);
+        int trueIndex = 0;
+        for (int i = 0; i < Main.distinctTileCount; i++) {
+            if (this.possibilities[i]) {
+                if (randomIndex == trueIndex) {
+                    this.collapsed = true;
+                    this.possibilities = new boolean[Main.distinctTileCount];
+                    this.color = i;
+                    this.possibilities[i] = true;
+                }else {
+                    trueIndex++;
+                }
+            }
+        }
+    }
+    public void update() {
+        if (this.possibilitiesSum == 1) {
+            this.collapse();
+        }else if (this.possibilitiesSum == 0) {
+            System.out.println("ERROR: Cell " + String.valueOf(this.x) + "," + String.valueOf(this.y) + " has 0 possibilities.");
+        }
+    }
 
+    public boolean demand() {
+         boolean changed = false;
+         for (int i = 0; i < Main.distinctTileCount; i++) {
+             if (this.possibilities[i] && !this.demands[i]) {
+                 changed = true;
+                 this.possibilities[i] = false;
+                 this.possibilitiesSum -= 1;
+             }
+         }
+         this.update();
+         return changed;
+    }
+    public String printValue() {
+        if (!this.collapsed) {return "n";}
+        return Main.colorConvert[this.color];
     }
 
 }
--- src/WaveFunctionCollapse/Main.java
+++ src/WaveFunctionCollapse/Main.java
@@ -2,18 +2,116 @@ package WaveFunctionCollapse;
 
 public class Main {
     public static int distinctTileCount = 2;
-    public final static int outputImageWidth = 10;
-    public final static int outputImageHeight = 10;
+    public final static int kernelSize = 3;
+    public final static int[][][] kernelList = new int[][][] {{
+            {0, 1, 0},
+            {1, 0, 1},
+            {0, 1, 0}
+    }, {
+            {1, 0, 1},
+            {0, 1, 0},
+            {1, 0, 1}
+    }};
+    public final static String[] colorConvert = new String[] {".", "#"};
+    public final static int outputImageWidth = 8;
+    public final static int outputImageHeight = 8;
     public static Cell[][] cellArray = new Cell[outputImageWidth][outputImageHeight];
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(Math.random());
-        }
         for (int x = 0; x < outputImageWidth; x++) {
             for (int y = 0; y < outputImageHeight; y++) {
                 cellArray[x][y] = new Cell(x, y);
+
+            }
+        }
+        cellArray[2][2].collapse();
+        waveHandling(cellArray[2][2]);
+        consolePrint();
+    }
+
+    public static void consolePrint() {
+        for (int y = 0; y < outputImageWidth; y++) {
+            String printLine = "";
+            for (int x = 0; x < outputImageHeight; x++) {
+                printLine += cellArray[y][x].printValue();
+            }
+            System.out.println(printLine);
+        }
+    }
+
+
+    public static void waveHandling(Cell homeCell) {
+        for (int[][] kernel:kernelList) {
+            if (kernelTest(kernel, homeCell.x, homeCell.y)) {
+                appendDemands(kernel, homeCell.x, homeCell.y);
+            }
+        }
+        boolean[][] changedCells = pushDemands(homeCell.x, homeCell.y);
+        consolePrint();
+        System.out.println("Completed");
+        for (int y = 0; y < outputImageHeight; y++) {
+            for (int x = 0; x < outputImageHeight; x++) {
+                if (changedCells[y][x]) {
+                    System.out.println(String.valueOf(x) + "," + String.valueOf(y));
+
+                }
+            }
+        }
+        for (int y = 0; y < changedCells.length; y++) {
+            for (int x = 0; x < changedCells.length; x++) {
+                if (changedCells[y][x]) {
+                    System.out.println("Next homeCell: " + String.valueOf(x) + "," + String.valueOf(y) + ".");
+                    waveHandling(cellArray[y][x]);
+                }
+            }
+        }
+    }
+
+
+
+
+    public static boolean kernelTest(int[][] kernel, int homeX, int homeY) {
+        for (int y = 0; y < kernel.length; y++) {
+            for (int x = 0; x < kernel.length; x++) {
+                // If it doesn't match.
+                try {
+                    if (!cellArray[homeY+y-1][homeX+x-1].possibilities[kernel[y][x]]) {return false;}
+                }catch (IndexOutOfBoundsException e) {
+                    continue;
+                }
+
+            }
+        }
+        return true;
+    }
+
+    public static void appendDemands(int[][] kernel, int homeX, int homeY) {
+        for (int y = 0; y < kernel.length; y++) {
+            for (int x = 0; x < kernel.length; x++) {
+                // If it doesn't match.
+                try {
+                    cellArray[homeY+y-1][homeX+x-1].demands[kernel[y][x]] = true;
+                }catch (IndexOutOfBoundsException e) {
+                    continue;
+                }
+
             }
         }
 
+    }
+
+    public static boolean[][] pushDemands(int homeX, int homeY) {
+        boolean[][] changedCells = new boolean[outputImageHeight][outputImageWidth];
+        for (int y = 0; y < kernelSize; y++) {
+            for (int x = 0; x < kernelSize; x++) {
+                // If it doesn't match.
+                try {
+                    changedCells[homeY+y-1][homeX+x-1] = cellArray[homeY+y-1][homeX+x-1].demand();
+                }catch (IndexOutOfBoundsException e) {
+                    continue;
+                }
+
+            }
+        }
+        return changedCells;
     }
 }
